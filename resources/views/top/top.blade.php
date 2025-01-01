7cf1bba4d735c9b8ea7d67325391405c7f1c329c
ayano-0110 <gd.clbom@gmail.com>
2025-01-01 16:49:41 +0000

    app/topを連携

--- resources/views/top/top.blade.php
+++ resources/views/top/top.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.app')
 <!DOCTYPE html>
 <html>
     <head>
@@ -30,38 +31,27 @@
     <body>
       <header>
         <h1>Book Record</h1>
-        <nav>
-          <a href="#">ホーム</a>
-          <a href="#">サービス</a>
-          <a href="#">お問い合わせ</a>
-          <a href="#">ブログ</a>
-        </nav>
       </header>
 
       <div>
-        <h2>ページのコンテンツ</h2>
         <!-- フォームで送信される -->
-  　　　　　<form action="login.blade.php" method="POST">
+  　　　　　<form action="book.blade.php" method="POST">
   　　　　　　<button type="submit">ログイン</button>
   　　　　　</form>
-
-        <p>これはページのコンテンツ部分です。</p>
       </div>
 
 
       <div>
-        <h2>ページのコンテンツ</h2>
+        <h2>アカウントをお持ちでない方</h2>
         <!-- フォームで送信される -->
   　　　　　<form action="register.blade.php" method="POST">
   　　　　　　<button type="submit">新規登録</button>
   　　　　　</form>
-
-        <p>これはページのコンテンツ部分です。</p>
       </div>
 
 
       <footer>
-        <p>© 2025 Book Record. All Rights Reserved.</p>
+        <p>© 2025 Book Record.</p>
       </footer>
     </body>
 </html>
